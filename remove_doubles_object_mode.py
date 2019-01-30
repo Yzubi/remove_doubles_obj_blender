ddfa3ded0221570f17295e07de14d9ffc3e41ab7
Yzubi <42348748+Yzubi@users.noreply.github.com>
2019-01-30 13:59:18 -0200

    Create remove_doubles_object_mode.py

---
+++ remove_doubles_object_mode.py
@@ -0,0 +1,21 @@
+import bpy
+import bmesh
+
+context = bpy.context
+
+distance = 0.0 # remove doubles tolerance.
+if True: #def execute(self, context):
+
+    meshes = [o.data for o in context.selected_objects
+                      if o.type == 'MESH']
+
+    bm = bmesh.new()
+
+    for m in meshes:
+        bm.from_mesh(m)
+        bmesh.ops.remove_doubles(bm, verts=bm.verts, dist=distance)
+        bm.to_mesh(m)
+        m.update()
+        bm.clear()
+
+    bm.free()
